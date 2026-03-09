c056a2b86bcf265ae03cfc47c5a7f70181d9b81d
Tasnim A. <tasu2301@gmail.com>
2026-03-09 20:12:38 +0600

    start Numpy - day_1

---
+++ Numpy/mean.py
@@ -0,0 +1,15 @@
+import numpy as np
+# values = [10, 20, 30]
+
+# count = len(values)
+# total = sum(values)
+# mean = total/count
+
+# print(mean)
+
+
+values = np.array([15, 42, 7, 93, 28, 61])
+
+print(np.mean(values))
+print(np.min(values))
+print(np.max(values))
