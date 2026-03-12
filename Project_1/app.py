abb076b89fcc34ad03a07edffff3fa9482503211
Tasnim A. <tasu2301@gmail.com>
2026-03-12 08:17:58 +0600

    added edit feature

--- Project_1/app.py
+++ Project_1/app.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input('Type add, show or exit: ')
+    user_action = input('Type add, show, edit or exit: ')
     user_action = user_action.strip().lower()
 
     match user_action:
@@ -13,6 +13,13 @@ while True:
             for item in todos:
                 serial = todos.index(item) + 1
                 print(serial, item)
+        case 'edit':
+            edit_option = int(input('Number of the todo to edit: '))
+            position = int(edit_option) - 1
+            print(todos[position])
+            new_todo = input('Enter the new todo: ')
+            todos[position] = new_todo
+            print('todos modified')
         case 'exit':
             break
         case _:
